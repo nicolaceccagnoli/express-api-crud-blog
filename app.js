41026fb5b30a39aba86618361aa9a0413e7a526d
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-05-28 17:19:54 +0200

    add middleware

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ const port = 3000;
 
 // Invoco il controller
 const postsRouter = require("./routers/posts.js");
-
-const { path } = require('./utils.js');
+// Importo il middleware per le richieste HTTP
+const routersLogger = require('./middleware/routersLogger.js');
+// Importo il middleware per la gestione degli errori
+const errorsFormatter = require('./middleware/errorsFormatter.js');
+// Importo il middleware la gestione della rotta non trovata
+const routesNotFound = require('./middleware/routesNotFound.js');
 
 // Middleware per i file statici
 app.use(express.static('./public'));
+
+// Utilizzo il middleware per le richieste HTTP
+app.use(routersLogger);
 
 // application/json
 app.use(express.json());
@@ -20,6 +27,12 @@ app.use(express.urlencoded({ extended: true }));
 
 // Definisco le mie rotte
 app.use('/posts', postsRouter);
+
+// Utilizzo il middleware per le rotte non trovate
+app.use(routesNotFound);
+
+// Utilizzo il middleware per la gestione degli errori
+app.use(errorsFormatter);
 
 app.listen(port, () => {
     console.log(`Server avviato su http://localhost:${port}`);
