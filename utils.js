dad9af20d37adc4580996eb8c12efb2daa9596b9
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-05-28 16:15:45 +0200

    fixed createSlug

--- utils.js
+++ utils.js
@@ -1,6 +1,9 @@
 // Importo Path per creare percorsi ai file
 const path = require('path');
 
+// Importo i posts
+let posts = require('./db/posts.json');
+
 // Importo File System
 const fs = require("fs");
 
@@ -8,8 +11,23 @@ const fs = require("fs");
 const slugify = require('slugify');
 
 // Creo una funzione per creare lo slug dei post
+// const createSlug = (name) => {
+//     slugify(name, {lower: true, strict: true});
+// }
+
 const createSlug = (name) => {
-    slugify(name, {lower: true, strict: true});
+    // Definisco uno slug di base
+    const slugBase = name.replaceAll(' ', '-').toLowerCase().replaceAll('/', '');
+    const slugs = posts.map(p => p.slug);
+    // Inizializzo un contatore
+    let counter = 1;
+    let slug = slugBase;
+    // fintanto che lo slug è incluso negli slugs
+    while(slugs.includes(slug)) {
+        slug = `${slugBase}-${counter}`;
+        counter++;
+    }
+    return slug;
 }
 
 // Creo una funzione per l'eliminazione del file da public
